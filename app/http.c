826dd8969b9cfb57234f4d09198fb3fe75e22a00
dporr <diego.porras@owasp.org>
2024-01-11 12:43:57 +0000

    REFACTOR: Moved implementations from http.h -> http.c

---
+++ app/http.c
@@ -0,0 +1,19 @@
+#include "http.h"
+ssize_t send_response(int client_socket, struct response my_r){
+    //check socket fd is valid
+    char *response = malloc(
+                        strlen(my_r.status_line.http_version) +
+                        strlen(my_r.status_line.reason_phrase) +
+                        6);
+    sprintf(response, "%s %s\r\n\r\n",
+                     my_r.status_line.http_version,
+                     my_r.status_line.reason_phrase
+                     );
+    printf("Size of my_r: %lu size of response: %lu \nContents of response: %s",
+            sizeof(my_r),
+            sizeof(response),
+            response
+            );
+   ssize_t  ret =  send(client_socket,response, strlen(response), 0);
+   return ret;
+}
